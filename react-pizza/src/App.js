2e24955e46fdff9046b4e1153b6f78bfc027bebb
arrow767 <i.silnov.anton@gmail.com>
2020-06-23 11:26:56 +0400

    Урок 3

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -15,7 +15,7 @@ function App() {
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <Categories />
+            <Categories items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']} />
             <div className="sort">
               <div className="sort__label">
                 <svg
--- react-pizza/src/components/Categories.jsx
+++ react-pizza/src/components/Categories.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-function Categories() {
+function Categories({ items }) {
   return (
     <div className="categories">
       <ul>
         <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {items.map((name, index) => (
+          <li key={`${name}_${index}`}>{name}</li>
+        ))}
       </ul>
     </div>
   );
