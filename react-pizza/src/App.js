173c716b57522bd4895c34c8cd9d6b421ea90412
Антон Сильнов <antonsilnov@MacBook-Pro-Anton.local>
2020-06-25 14:10:34 +0400

    Урок 3

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -15,7 +15,10 @@ function App() {
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <Categories items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']} />
+            <Categories
+              onClickItem={(name) => console.log(name)}
+              items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']}
+            />
             <div className="sort">
               <div className="sort__label">
                 <svg
--- react-pizza/src/components/Categories.jsx
+++ react-pizza/src/components/Categories.jsx
@@ -1,12 +1,73 @@
 import React from 'react';
 
-function Categories({ items }) {
+
+// Пример: классовый компонент
+// class Categories extends React.Component {
+//   state = {
+//     activeItem: null,
+//     test: 123,
+//   }
+
+//   onSelectItem = index => {
+//     this.setState({
+//       activeItem: index,
+//     })
+//   }
+
+
+//   render(){
+//     const {items, onClickItem} = this.props;
+    
+//     return(
+//           <div className="categories">
+//       <ul>
+//         <li>Все</li>
+//         {items.map((name, index)=> (
+//         <li 
+//         className={this.state.activeItem === index ? 'active' : ''}
+//         onClick = {() => this.onSelectItem(index)} 
+//         key={`${name}_${index}`}>{name}
+//         </li>
+//         ))}
+//       </ul>
+//     </div>
+//     )
+//   }
+// }
+
+function Categories({ items, onClickItem }) {
+  
+  const [activeItem, setActiveItem] = React.useState(null)
+
+  const [count, setCount] = React.useState(null)
+  
+
+  const onSelectItem = (index) => {
+    setActiveItem(index)
+    // setCount((num) => num + 1)
+    // console.log(setCount);
+    
+  }
+
+  // const state = React.useState(null)
+  // const activeItem = state[0]
+  // const setActiveItem = state[1]
+  // console.log(state);
+  
   return (
     <div className="categories">
+      
       <ul>
-        <li className="active">Все</li>
-        {items.map((name, index) => (
-          <li key={`${name}_${index}`}>{name}</li>
+      {/* <h3>{count}</h3> */}
+        <li
+        onClick={()=> onSelectItem(null)}
+        className={activeItem === null ? 'active' : ''}>Все</li>
+        {items && items.map((name, index) => (
+          <li 
+          className = {activeItem === index ? 'active' : ''}
+          onClick={() => onSelectItem(index)} key={`${name}_${index}`}>
+            {name}
+          </li>
         ))}
       </ul>
     </div>
