8a3724b5a78d676d7bad8d4bdbef91065bc4485c
arrow767 <i.silnov.anton@gmail.com>
2020-08-20 15:22:21 +0400

    Урок 7

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import store from './redux/store'
-import {setPizzas as setPizzasAction} from './redux/actions/pizzas'
-import {connect} from 'react-redux'
+import {setPizzas} from './redux/actions/pizzas'
+import {useSelector, useDispatch} from 'react-redux'
 
 // import logo from './logo.svg';
 // import './App.css';
@@ -11,56 +10,47 @@ import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
 
-// function App() {
+function App(){
+    const dispatch = useDispatch()
+    const {items} = useSelector(({pizzas, filters}) => {
+        return({
+            items: pizzas.items,
+            sortBy: filters.sortBy
+        })
+
+    })
+    console.log(items)
+    React.useEffect(()=> {
+            axios.get('http://localhost:3000/db.json').then(({ data }) => {
+                dispatch(setPizzas(data.pizzas))
+            });
+    },[])
+    return(
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <Route path="/" render={() => <Home items={items} />} exact />
+                <Route path="/cart" component={Cart} exact />
+            </div>
+        </div>
+    )
+}
+
+export default App
+
+
+// const mapStateToProps = state => {
+//     return {
+//         items: state.pizzas.items,
+//         filters: state.filters
+//     }
+// }
 //
-//   React.useEffect(() => {
-//     axios.get('http://localhost:3000/db.json').then(({ data }) => {
-//       setPizzas(data.pizzas);
-//     });
-//     // fetch('http://localhost:3000/db.json')
-//     //   .then((resp) => resp.json())
-//     //   .then((json) => {
-//     //     setPizzas(json.pizzas);
-//     //   });
-//   }, []);
-//
-//
-//   return
+// const mpDispatchToProps = dispatch =>{
+//     return {
+//         setPizzas: (items)=> dispatch(setPizzasAction(items)), dispatch
+//     }
 // }
 
-class App extends React.Component{
-    componentDidMount() {
-        axios.get('http://localhost:3000/db.json').then(({ data }) => {
-            this.props.setPizzas(data.pizzas)
-    });
-    }
-
-    render(){
-        console.log(this.props)
-        return (
-            <div className="wrapper">
-                <Header />
-                <div className="content">
-                    <Route path="/" render={() => <Home items={this.props.items} />} exact />
-                    <Route path="/cart" component={Cart} exact />
-                </div>
-            </div>
-        );
-    }
-}
-
-const mapStateToProps = state => {
-    return {
-        items: state.pizzas.items,
-        filters: state.filters
-    }
-}
-
-const mpDispatchToProps = dispatch =>{
-    return {
-        setPizzas: (items)=> dispatch(setPizzasAction(items)), dispatch
-    }
-}
-
-
-export default connect(mapStateToProps, mpDispatchToProps)(App);
+//
+// export default connect(mapStateToProps, mpDispatchToProps)(App);
