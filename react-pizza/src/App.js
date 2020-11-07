c5001bd8071d6b2ddd5a383db40e0044c8c75e48
arrow767 <i.silnov.anton@gmail.com>
2020-11-08 00:50:19 +0400

    Урок 7(4)

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -16,8 +16,8 @@ function App(){
     // console.log(window.store.getState())
     // console.log(items)
     React.useEffect(()=> {
-            axios.get('http://localhost:3000/db.json').then(({ data }) => {
-                dispatch(setPizzas(data.pizzas))
+            axios.get('http://localhost:3001/pizzas').then(({ data }) => {
+                dispatch(setPizzas(data))
             });
     },[])
     return(
