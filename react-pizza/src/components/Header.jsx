7fac044ce5896b097526829665712d1b00ddef1e
arrow767 <i.silnov.anton@gmail.com>
2020-11-21 16:07:36 +0400

    Урок 9(1|cart(54m))

--- react-pizza/src/components/Header.jsx
+++ react-pizza/src/components/Header.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logoSvg from '../assets/img/pizza-logo.svg';
 import Button from './Button';
+import {useSelector} from "react-redux";
 
 function Header() {
+  const {totalPrice, totalCount} = useSelector(({cart})=>cart)
   return (
     <div className="header">
       <div className="container">
@@ -19,7 +21,7 @@ function Header() {
         <div className="header__cart">
           <Link to="/cart">
             <Button className="button--cart">
-              <span>520 ₽</span>
+              <span>{totalPrice}₽</span>
               <div className="button__delimiter"></div>
               <svg
                 width="18"
@@ -49,7 +51,7 @@ function Header() {
                   strokeLinejoin="round"
                 />
               </svg>
-              <span>3</span>
+              <span>{totalCount}</span>
             </Button>
           </Link>
         </div>
--- react-pizza/src/redux/reducers/index.js
+++ react-pizza/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux";
-import filtersReducers from './filters';
-import pizzasReducer from './pizzas'
+import filters from './filters';
+import pizzas from './pizzas'
+import cart from './cart'
 
 const rootReducer = combineReducers({
-    filters:filtersReducers,
-    pizzas:pizzasReducer
+    filters,
+    pizzas,
+    cart
 })
 
 export default rootReducer
